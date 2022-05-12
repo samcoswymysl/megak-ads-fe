7db8d415ee5773fb10dbd196387a8778de129707
samcoswymysl <rafalkrukowskipraca123@gmail.com>
2022-05-12 19:52:54 +0200

    Basic structure og main vue

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import './App.css'
 
 
 export const App = () => {
-  return (<>test</>);
+  return (
+      <>
+        <header>
+            <h1><strong>Mega</strong> ogłoszenia</h1>
+            <button>Dodaj ogłoszenie</button>
+            <div className="search">
+                <input type="text"/>
+                <button>Szukaj</button>
+            </div>
+        </header>
+          <div className="map">...</div>
+      </>
+  );
 }
 
 

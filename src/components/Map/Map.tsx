6931037aab0d48bcd28a8c946fe9d8fd87b6a5d0
samcoswymysl <rafalkrukowskipraca123@gmail.com>
2022-05-13 18:17:39 +0200

    Fixed bugs in leaflet marker icon

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 import 'leaflet/dist/leaflet.css'
+
 import '../../utils/fix-map-icons'
 
 import './Map.css'
